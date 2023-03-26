80e768f1d70db59e8ccc540de91dd0401c5abcae
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2023-03-26 18:17:39 +0200

    Leverage next/image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import { api, type RouterOutputs } from "~/utils/api";
-
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 
 dayjs.extend(relativeTime);
 
@@ -15,10 +15,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="Profile image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -34,10 +36,12 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div className="flex items-center gap-x-3 border-b border-slate-400 p-4">
-      <img
+      <Image
         src={author.profileImageUrl}
-        alt="Profile image"
         className="h-10 w-10 rounded-full"
+        alt={`@${author.username}'s profile picture`}
+        width={40}
+        height={40}
       />
       <div className="flex flex-col">
         <div className="flex gap-x-1 text-slate-300">
